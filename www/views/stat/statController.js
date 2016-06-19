8af5ffa5c981f4e64da426c7117301cc76cc20c5
B-Massy <cm_bourennani@esi.dz>
2016-06-19 02:10:03 +0000

    liaison des statistique avec le backend

--- www/views/stat/statController.js
+++ www/views/stat/statController.js
@@ -1,5 +1,42 @@
 angular.module('App')
-.controller('StatController',function($scope,StatService){
+.controller('StatController',function($scope,StatService,DepensesService,EntreesService){
+
+
+    var userId = 2;
+
+    var getDataFromBackend = function (userId){
+
+      DepensesService.getDepenseOneUser(userId)
+        .success(function(depenses){
+
+          console.log("depenses");
+          console.log(depenses);
+          StatService.lineChartDataSetDepense = StatService.formateTransactionsToLineChartDataSet(depenses);
+          console.log(StatService.lineChartDataSetDepense);
+          $scope.changeChart($scope.selectedChart.nom);
+        })
+        .error(function(error){
+
+          console.log("getDepensesOneUser error");
+          console.log(error);
+        });
+
+      EntreesService.getEntreesOneUser(userId)
+        .success(function(entrees){
+
+          console.log("entrees");
+          console.log(entrees);
+          StatService.lineChartDataSetEntree = StatService.formateTransactionsToLineChartDataSet(entrees);
+          console.log(StatService.lineChartDataSetEntree);
+        })
+        .error(function(error){
+
+          console.log("getEntreesOneUser error");
+          console.log(error);
+
+        });
+
+    };
 
     $scope.chartTypes = ['Depenses','Entrees','Entrees VS Depenses'];
 
@@ -7,57 +44,47 @@ angular.module('App')
       nom: $scope.chartTypes[0]
     };
 
-
-    $scope.$watch('selectedChart.nom',function(newVal,oldVal){
-
-        $scope.changeChart(newVal);
-    });
-
-
-
     Highcharts.theme = {
-        colors: [],
-        legend: {
-            itemStyle: {
-                font: '9pt Trebuchet MS, Verdana, sans-serif',
-                color: 'black'
-            },
-            itemHoverStyle: {
-                color: 'gray'
-            }
+      colors: [],
+      legend: {
+        itemStyle: {
+          font: '9pt Trebuchet MS, Verdana, sans-serif',
+          color: 'black'
         },
-        lang: {
-            decimalPoint: ',',
-            thousandsSep: ' '
+        itemHoverStyle: {
+          color: 'gray'
         }
+      },
+      lang: {
+        decimalPoint: ',',
+        thousandsSep: ' '
+      }
     };
 
     $scope.chart = {
-        options: {
-            chart:{
-                type:'line'
-            },
-            legend:{
-                enabled: true
-            }
+      options: {
+        chart:{
+          type:'line'
         },
-        title:{
-            text:null
-        },
-        yAxis:{
-            title:'Montant (DA)',
+        legend:{
+          enabled: true
+        }
+      },
+      title:{
+        text:null
+      },
+      yAxis:{
+        title:'Montant (DA)'
 
-        },
-        xAxis:{
-            type:'datetime'
-        },
-        series:[] /*data to define */
+      },
+      xAxis:{
+        type:'datetime'
+      },
+      series:[] /*data to define */
     };
 
     $scope.changeChart = function (charteType){
 
-
-      //--TODO getting data from the backend
       if(charteType == 'Depenses' ){
 
         Highcharts.theme.colors = ['#ff0000'];
@@ -79,6 +106,17 @@ angular.module('App')
         $scope.chart.series.push(StatService.getSerieFromLineChartDataSet(StatService.lineChartDataSetDepense,'Depenses'));
         $scope.chart.series.push(StatService.getSerieFromLineChartDataSet(StatService.lineChartDataSetEntree,'Entrees'));
       }
-    }
+    };
+
+
+    getDataFromBackend(userId);
+
+
+    $scope.$watch('selectedChart.nom',function(newVal,oldVal){
+
+        $scope.changeChart(newVal);
+    });
+
+
 
 });
--- www/views/stat/statService.js
+++ www/views/stat/statService.js
@@ -1,15 +1,13 @@
 angular.module('App')
 .service('StatService',function(){
 
-    var LineChartDataItem= function (date, value) {
+    var LineChartDataItem = function (date, value) {
 
         this.date = date ;
         this.value = value;
     };
 
-    
 
-    //--TODO getting data from the backend
     var lineChartDataSetDepense = []; /*[
         new LineChartDataItem(new Date('2016-05-08'),100.25),
         new LineChartDataItem(new Date('2016-05-09'),150.55),
@@ -17,26 +15,39 @@ angular.module('App')
         new LineChartDataItem(new Date('2016-05-010'),50.23)
     ];*/
 
-    var lineChartDataSetEntree = [] ; /* [
+    var lineChartDataSetEntree = []; /* [
         new LineChartDataItem(new Date('2016-05-08'),200),
         new LineChartDataItem(new Date('2016-05-09'),300.55),
         new LineChartDataItem(new Date('2016-05-010'),50.00),
         new LineChartDataItem(new Date('2016-05-010'),100.23)
     ];*/
-  
-    
-    
-    var formateTransactionToLineDataSetEntree = function(transactions){
-      
+
+
+
+    var formateTransactionToLineDataSet = function(transactions){
+
       var dataSet = [];
+      var date, day, month, year, dateArray ,  j = 0;
+
       for(var i = 0 ; i < transactions.length ; i++ ){
-       
-        dataSet[i] = new LineChartDataItem(new Date(transactions[i].dateCreation),transactions[i].montant);
+
+        if(transactions[i].dateCreation){
+
+          dateArray =  transactions[i].dateCreation.toString().split("-");
+          year = dateArray[0];
+          month = dateArray[1];
+          day = dateArray[2];
+          date = year+"-"+month+"-"+day ;
+          dataSet[j] = new LineChartDataItem(new Date(date),transactions[i].montant);
+          j++;
+        }
       }
-      return dataSet;
-      
+      console.log("dataSet from FormateTransactionToLineDataSet");
+      console.log(dataSet);
+      return sorte(dataSet);
+
     };
-  
+
     var getSerieFromLineChartDataSet = function(lineChartDataSet,serieName){
 
         var serie = {
@@ -50,8 +61,11 @@ angular.module('App')
         *           [dateN,valN]
         *              ]
         * */
+        console.log("line chart data set getSerieFromLineChartDataSet");
+        console.log(lineChartDataSet);
         for(var i = 0 ; i < lineChartDataSet.length ; i ++){
 
+            console.log(lineChartDataSet[i]);
             if(lineChartDataSet[i].date && lineChartDataSet[i].value > 0){// les deux sont valides
                 serie.data.push([lineChartDataSet[i].date.getTime(),lineChartDataSet[i].value]);
             }
@@ -59,8 +73,28 @@ angular.module('App')
         return serie ;
     };
 
+    var sorte = function(dataItems){
+      var permut = true;
+      var tmp;
+
+      while( permut ){
+
+        permut = false ;
+        for(var i = 0 ; i < dataItems.length - 1 ; i ++){
+
+          if(dataItems[i].date > dataItems[i+1].date ){
+            tmp = dataItems[i];
+            dataItems[i] = dataItems[i+1];
+            dataItems[i+1] = tmp;
+            permut = true;
+          }
+        }
+      }
+      return dataItems;
+    };
+
     return{
-        formateTransactionsToLineChartDataSetEntree: formateTransactionToLineDataSetEntree,
+        formateTransactionsToLineChartDataSet: formateTransactionToLineDataSet,
         lineChartDataSetDepense: lineChartDataSetDepense,
         lineChartDataSetEntree : lineChartDataSetEntree,
         getSerieFromLineChartDataSet: getSerieFromLineChartDataSet
